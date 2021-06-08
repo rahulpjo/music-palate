cb2717967cc3fdc64cbf2137d3c95eb9fa322aca
rahulpjo <rjoshi9000@gmail.com>
2021-06-08 14:56:38 -0700

    JavaScript and some pseudo-elements

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ const searchForm = document.getElementById("search-artist");
 const searchInput = document.getElementById("search-area");
 const message = document.querySelector("p");
 const recommendations = document.getElementById("recommendations");
+const additionalInfo = document.querySelector(".more-info");
+
+const removeClass = () => {
+  document.querySelectorAll(".artist").forEach((artist) => {
+    artist.classList.remove("show");
+  });
+};
+
+const addListeners = () => {
+  document.querySelectorAll(".artist").forEach((artist) => {
+    artist.addEventListener("click", (e) => {
+      // removeClass();
+      console.log(e);
+      if (e.target.localName === "h3") {
+        e.target.parentNode.classList.toggle("show");
+      } else {
+        e.target.classList.toggle("show");
+      }
+      additionalInfo.classList.toggle("show-info");
+    });
+  });
+};
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -12,6 +34,7 @@ searchForm.addEventListener("submit", (e) => {
   for (let i = 1; i < 5; i++) {
     createArtistBlock(`Artist ${i}`);
   }
+  addListeners();
 });
 
 const clearArea = () => {
@@ -26,31 +49,6 @@ const createArtistBlock = (name) => {
   const artistName = document.createElement("h3");
   artistName.innerHTML = `${name} <i class="fas fa-caret-down"></i>`;
 
-  // const artistMore = document.createElement("div");
-  // artistMore.classList.add("dropdown");
-
-  // artistName.addEventListener("click", () => {
-  //   artistMore.classList.toggle("show-dropdown");
-  // });
-
-  // const artistInfo = document.createElement("div");
-  // const artistInfoHeader = document.createElement("h5");
-  // const artistInfoFull = document.createElement("p");
-  // artistInfoHeader.innerHTML = "Information:";
-  // artistInfoFull.innerHTML = "Lorem ipsum dolor sit amet, consectetur";
-  // artistInfo.append(artistInfoHeader, artistInfoFull);
-  // artistMore.appendChild(artistInfo);
-
-  // const artistVideo = document.createElement("div");
-  // artistVideo.innerHTML = `<iframe width="560" height="315" src="" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-  // artistMore.appendChild(artistVideo);
-
-  // const artistSonglist = document.createElement("div");
-  // artistSonglist.innerHTML =
-  //   "<h5>Top Songs:</h5><ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>";
-  // artistMore.appendChild(artistSonglist);
-
   artist.appendChild(artistName);
-  // artist.appendChild(artistMore);
   recommendations.appendChild(artist);
 };
